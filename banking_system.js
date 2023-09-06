94f9de72318338bfe8ff2e5a490487811f967431
iamtaufik <muhtopik07@gmail.com>
2023-09-06 22:41:11 +0700

    menambahkan file html

--- banking_system.js
+++ banking_system.js
@@ -1,30 +1,3 @@
 import { BankAccount } from './bank_account.js';
 
 const account = new BankAccount('John Doe', 1000);
-
-async function run() {
-  try {
-    // Skenario Testing
-
-    // Skenario 1 -> Berhasil melakukan deposit
-    const balance = await account.deposit(500);
-    console.log(`Deposit 500, balance: ${balance}`);
-
-    // Skenario 2 -> Gagal melakukan deposit karena amount tidak valid
-    await account.deposit(-500);
-
-    // Skenario 3 -> Berhasil melakukan withdraw
-    const balance2 = await account.withdraw(200);
-    console.log(`Withdraw 200, balance: ${balance2}`);
-
-    // Skenario 4 -> Gagal melakukan withdraw karena amount tidak valid
-    await account.withdraw(-200);
-
-    // Skenario 5 -> Gagal melakukan withdraw karena saldo kurang
-    await account.withdraw(10000);
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-run();
